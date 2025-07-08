35e655cdf36d8d2ebe739cb6c3816bbbddca7952
yash mittal <mittalyash616@gmail.com>
2025-07-08 22:11:15 +0530

    update

--- Sms_Spam_Detection/app.py
+++ Sms_Spam_Detection/app.py
@@ -4,4 +4,40 @@ from sklearn.preprocessing import LabelEncoder
 import nltk
 import pickle
 dataset=pickle.load(open('sms.pkl', 'rb'))
-print(dataset.head())
+dataset.drop_duplicates(keep='first',inplace=True)
+from sklearn.feature_extraction.text import CountVectorizer, TfidfVectorizer
+cv=CountVectorizer()
+tfidf=TfidfVectorizer()
+#x=cv.fit_transform(dataset['text']).toarray()
+x=tfidf.fit_transform(dataset['text']).toarray()
+y=dataset['target'].values
+print(y)
+from sklearn.model_selection import train_test_split
+x_train,x_test,y_train,y_test=train_test_split(x,y,test_size=0.2,random_state=2)
+from sklearn.naive_bayes import MultinomialNB,GaussianNB,BernoulliNB
+from sklearn.metrics import accuracy_score,confusion_matrix,precision_score
+gnb=GaussianNB()
+mnb=MultinomialNB()
+bnb=BernoulliNB()
+
+gnb.fit(x_train,y_train)
+y_pred1=gnb.predict(x_test)
+print(accuracy_score(y_test,y_pred1))
+print(confusion_matrix(y_test,y_pred1))
+print(precision_score(y_test,y_pred1))
+
+
+mnb.fit(x_train,y_train)
+y_pred2=mnb.predict(x_test)
+print(accuracy_score(y_test,y_pred2))
+print(confusion_matrix(y_test,y_pred2))
+print(precision_score(y_test,y_pred2))
+
+
+bnb.fit(x_train,y_train)
+y_pred3=bnb.predict(x_test)
+print(accuracy_score(y_test,y_pred3))
+print(confusion_matrix(y_test,y_pred3))
+print(precision_score(y_test,y_pred3))
+
+#tfidf and mnb
