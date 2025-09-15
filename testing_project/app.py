c5a236014a6408f10404ce432193b8f5df2c2fe8
yash mittal <mittalyash616@gmail.com>
2025-09-16 00:09:06 +0530

    updated

--- testing_project/app.py
+++ testing_project/app.py
@@ -47,3 +47,7 @@ async def chatbot(request: UserRequest):
     result = chain.invoke(message)
     
     return {"reply": result}
+if __name__ == "__main__":
+    import uvicorn
+    port = int(os.environ.get("PORT", 8000))  # must use $PORT
+    uvicorn.run(app, host="0.0.0.0", port=port)
